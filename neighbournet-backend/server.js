925eb65513cf01c7d01a6d008f4b56ea05e499ff
AdityaGit-1 <tiwariaditya199153@gmail.com>
2026-08-11 12:30:15 +0530

    feat: add AI locality digest with node-cron scheduling and Redis caching

--- neighbournet-backend/server.js
+++ neighbournet-backend/server.js
@@ -4,6 +4,7 @@ const app = require('./src/app');
 const connectDB = require('./src/config/db');
 require('./src/config/redis');
 const { initSocket } = require('./src/services/socket.service');
+const { startCronJobs } = require('./src/services/cron.service');
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,5 +12,8 @@ const httpServer = http.createServer(app);
 initSocket(httpServer);
 
 connectDB().then(() => {
-  httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  httpServer.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    startCronJobs();
+  });
 });
